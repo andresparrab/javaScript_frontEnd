305dfc40396c088f58e9bab7f24a8d9e679eb2f1
andresparrab <andres.parrab@gmail.com>
2022-10-28 05:33:45 +0200

    trying code for code testing

---
+++ JSCodeTesting/script.js
@@ -0,0 +1,38 @@
+// let name = prompt("andvändarnamn: ")
+// let password = prompt("Lösenord: ")
+
+// const person = {
+//   name: "andres",
+//   password: "123456",
+//   adress: "Home 125",
+//   mobile: 0703454567,
+//   content: function() {
+//     return this.name + " " + this.adress + " " + this.mobile;
+//   }
+// }
+
+// while (name != person.name | password != person.password) {
+
+//   if (name == person.name) {
+//     if (password == person.password) {
+//       console.log(`welcome: ${person.name}, with mobilenr: ${person.mobile}`)
+//     }
+
+//     else {
+//       console.log(`The password is incorrect`)
+//     }
+//   }
+//   else {
+//     console.log(`The username: ${person.name} does not exist`)
+//   } name = prompt("andvändarnamn: ")
+//   password = prompt("Lösenord: ")
+
+// }
+//
+
+console.time();
+for (let index = 0; index < 1000; index++) {
+console.log("hej");
+
+}
+  console.timeEnd();
