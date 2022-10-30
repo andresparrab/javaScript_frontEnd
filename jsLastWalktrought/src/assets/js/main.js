dfd3457e4cfffd2af8c9abc159886e15571f4a83
andresparrab <andres.parrab@gmail.com>
2022-10-30 12:37:53 +0100

    added last walktrougt

---
+++ jsLastWalktrought/src/assets/js/main.js
@@ -0,0 +1,7 @@
+let nav = document.createElement('nav')
+let para = document.createElement('p')
+let textnode = document.createTextNode("this inseide the nav")
+para.appendChild(textnode)
+let body = document.querySelector('body')
+nav.append(para)
+body.appendChild(nav)
