6556970d1b0d31f7a01e99845d08d4275c8df42a
andresparrab <andres.parrab@gmail.com>
2022-10-30 06:16:20 +0100

    added lars test examples folder

--- JSCodeTesting/JestTesting/array.test.js
+++ JSCodeTesting/JestTesting/array.test.js
@@ -3,7 +3,7 @@ const cloneArray = require('./array')
 describe('the ultimate test array', () => {
   test('lets tst this fucker', () => {
     const array = [1, 2, 3];
-    expect(cloneArray(array)).toBe(array);
+    expect(cloneArray(array)).not.toBe(array);
     expect(cloneArray(array)).toEqual(array);
   })
 })
