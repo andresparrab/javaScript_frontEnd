d5506c958d52a6b174b6c1538225e71c6d814d3a
andresparrab <andres.parrab@gmail.com>
2022-10-31 22:07:17 +0100

    added c-fetch-modal parcel-version

--- jsLastWalktrought/src/assets/js/main.js
+++ jsLastWalktrought/src/assets/js/main.js
@@ -1,7 +1,9 @@
+alert("hello2")
 let nav = document.createElement('nav')
 let para = document.createElement('p')
 let textnode = document.createTextNode("this inseide the nav")
 para.appendChild(textnode)
-let body = document.querySelector('body')
+let body = document.querySelector('body').children[0]
+console.log(body)
 nav.append(para)
-body.appendChild(nav)
+body.before(nav)
